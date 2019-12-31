9799660de35aaeb6f71775babd93840b620c8e40
Sacha Maire <sacha.maire18@imperial.ac.uk>
2019-12-31 17:36:45 +0100

    Now able to use socket: send txt file to server, and then server runs merlin
    Addeed internet permission to use the socket
    
    The socket is given as argument th IP address of my computer + a port: this port willbe the same on the JAVA SE program run on the PC
    
    The SendToServer class takes a string (the one entered by user) as input.
    Send it to the server. The part of the server code is alos coded in jave, but not android studio (using Intellij). I will try to publish it asap (I am able to run terminal commands, so it executes merlin :) )

--- app/src/main/java/com/example/edp/SadSelected.java
+++ app/src/main/java/com/example/edp/SadSelected.java
@@ -2,6 +2,8 @@ package com.example.edp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -16,10 +18,9 @@ import java.io.InputStreamReader;
 
 public class SadSelected extends AppCompatActivity {
 
-    private static final String TEXT_TO_PRONOUNCE= "text.txt";
+    private static final String TEXT_TO_PRONOUNCE = "text.txt";
 
     EditText mEditText;
-
 
 
     @Override
@@ -27,17 +28,17 @@ public class SadSelected extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sad_selected);
 
-        mEditText= findViewById(R.id.edit_text);
+        mEditText = findViewById(R.id.edit_text);
     }
 
-    public void save (View v){
+    public void save(View v) {
         String text = mEditText.getText().toString();
-        FileOutputStream fos= null;
+        FileOutputStream fos = null;
 
-        try{
-            fos=openFileOutput(TEXT_TO_PRONOUNCE,MODE_PRIVATE);
+        try {
+            fos = openFileOutput(TEXT_TO_PRONOUNCE, MODE_PRIVATE);
             fos.write(text.getBytes());
-            Toast.makeText(this,"Saved to"+getFilesDir()+"/"+TEXT_TO_PRONOUNCE,Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "Saved to" + getFilesDir() + "/" + TEXT_TO_PRONOUNCE, Toast.LENGTH_LONG).show();
 
             mEditText.getText().clear();
 
@@ -46,53 +47,60 @@ public class SadSelected extends AppCompatActivity {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
-        }
-        finally{
-            if (fos!=null){
-                try{
+        } finally {
+            if (fos != null) {
+                try {
                     fos.close();
 
-                }catch (IOException e){
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
         }
 
     }
-    public void load(View v){
-        FileInputStream fis=null;
+
+    public void load(View v) {
+        FileInputStream fis = null;
 
         try {
-            fis =openFileInput(TEXT_TO_PRONOUNCE);
-            InputStreamReader isr =new InputStreamReader(fis);
-            BufferedReader br= new BufferedReader(isr);
-            StringBuilder sb= new StringBuilder();
+            fis = openFileInput(TEXT_TO_PRONOUNCE);
+            InputStreamReader isr = new InputStreamReader(fis);
+            BufferedReader br = new BufferedReader(isr);
+            StringBuilder sb = new StringBuilder();
             String text;
 
-            while ((text = br.readLine())!=null) {
+            while ((text = br.readLine()) != null) {
                 sb.append(text).append("\n");
 
 
             }
             mEditText.setText(sb.toString());
 
-            } catch (FileNotFoundException e1) {
+        } catch (FileNotFoundException e1) {
             e1.printStackTrace();
-            } catch (IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
-            }
-            finally{
-                if (fis!=null){
-                    try{
-                        fis.close();
-                    }catch (IOException e){
+        } finally {
+            if (fis != null) {
+                try {
+                    fis.close();
+                } catch (IOException e) {
                     e.printStackTrace();
-                    }
+                }
             }
 
         }
     }
 
 
+    //now create an intend to call the function serve to server
+
+    public void Send(View V) {
+        SendToServer sendtoserver = new SendToServer();
+        sendtoserver.execute(mEditText.getText().toString());
+
+
+    }
 }
 
